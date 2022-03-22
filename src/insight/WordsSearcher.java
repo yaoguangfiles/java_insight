898f49832b796ff554afabddcd59df5066526a70
yaoguangfiles <yaoguangfiles@gmail.com>
2022-03-22 16:31:38 -0400

    commit

--- src/insight/WordsSearcher.java
+++ src/insight/WordsSearcher.java
@@ -1,7 +1,9 @@
 package insight;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,6 +11,9 @@ import java.util.Scanner;
 public class WordsSearcher {
 	
 	public static void main(String[] args) {
+		
+		StringBuilder stat = new StringBuilder();
+		int num_search = 0;
 		
 		while(true) {
 		
@@ -20,22 +25,25 @@ public class WordsSearcher {
 			if (word.equalsIgnoreCase("-1"))
 				break;
 			
-			System.out.printf("Y) word searched: %s\n", word);
+//			System.out.printf("Y) word searched: %s\n", word);
 		      
 	//		String input = "Russian";
 	//		String searched = input.toUpperCase();
 			
 			String searched = word.toUpperCase();
 			
-			System.out.printf("Y) searched: %s\n", searched);
+//			System.out.printf("Y) searched: %s\n", searched);
 			
 			// directory where the input files reside
 			final String dir = "/home/rootroot/main/yao_j/workplace_java/insight/src/insight/text/";
+//			final String file_result = "result.txt";
 			
 			ArrayList<String> fileNames = new ArrayList<String>();
 			fileNames.add("file2.txt");
 			fileNames.add("file0.txt");
 			fileNames.add("file1.txt");
+			
+			
 			
 			String line;
 			ArrayList<String> text = new ArrayList<String>();
@@ -58,12 +66,17 @@ public class WordsSearcher {
 				}
 			}
 			
-			System.out.printf("Y) Result: text.size(): %d\n", text.size());
+//			System.out.printf("Y) Result: text.size(): %d\n", text.size());
 			StringBuilder sb = new StringBuilder();
 			
 			int cnt = 0;
 			
+			System.out.println("");
+			
 			for (int i = 0; i < text.size(); ++i) {
+				
+				Boolean is_match = false;
+				
 				String l = text.get(i).toString();
 	
 				String l_up = l.toUpperCase();
@@ -88,25 +101,54 @@ public class WordsSearcher {
 							should_record = false;
 					
 					if (should_record) {
+						is_match = true;
 						++cnt;
-	//					System.out.printf("l_i: %d, lin: %s\n", i,l);
-	//					System.out.printf("idx: %d, str: %s\n", idx,searched);
 						
 						sb.replace(idx, idx+searched.length(), searched.toUpperCase());
-	//					System.out.printf("rep: %d, rep: %s\n", i,sb.toString());
 					}
-					
-	//				idx = l.indexOf(searched, idx+searched.length());
 	
 					idx = l_up.indexOf(searched, idx+searched.length());
 				}
-	//			System.out.printf("rep: %d, rep: %s\n", i,sb.toString());
-				System.out.printf("%s\n", sb.toString());
+
+				if (is_match)
+					System.out.printf("%s\n", sb.toString());
 			}
 			
-			System.out.printf("\nSummary): sub: %s, cnt: %d\n\n\n", searched,cnt);
+//			System.out.printf("\nSummary): search: %s, cnt: %d\n\n\n", searched,cnt);
+			
+			++num_search;
+			stat.append("searched: "+word+"\t, matched: "+Integer.toString(cnt)+"\n");
+			
+//			try {
+//			      FileWriter myWriter = new FileWriter(dir+file_result);
+//			      myWriter.write(word);
+//			      myWriter.write(cnt);
+//			      myWriter.close();
+//			      System.out.println("Successfully wrote to the file.");
+//			    } catch (IOException e) {
+//			      System.out.println("An error occurred.");
+//			      e.printStackTrace();
+//			    }
+			
+//			try {
+//			      File myObj = new File(dir+file_result);
+//			      if (myObj.createNewFile()) {
+//			        System.out.println("File created: " + myObj.getName());
+//			      } else {
+//			        System.out.println("File already exists.");
+//			      }
+//			    } catch (IOException e) {
+//			      System.out.println("An error occurred.");
+//			      e.printStackTrace();
+//			    }
 			
 		  }
+		
+		System.out.println("\n------ Summary of search: -------\n");
+		System.out.printf("Number of search: %d\n", num_search);
+//		System.out.println("Summary of search: ");
+		System.out.println(stat.toString());
+		System.out.println("----------------------------------\n");
 		
 	}
 
