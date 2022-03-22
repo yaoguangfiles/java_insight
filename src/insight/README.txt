447308d259945c30019de78b7c68f6481116d60e
yaoguangfiles <yaoguangfiles@gmail.com>
2022-03-22 15:31:38 -0400

    commit

--- src/insight/README.txt
+++ src/insight/README.txt
@@ -1,4 +1,8 @@
 
+A. Assumtion:
+Assume words consist of lowercase or uppercase alphabet letters only. A word connected with dash (-) will be automatically seperated into two words. For example, word "real-world" will be seperate into "real" and "world".
+
+
 How to compile and run the Java program:
 
 1. Compile with: 
--- src/insight/WordsSearcher.java
+++ src/insight/WordsSearcher.java
@@ -9,19 +9,21 @@ public class WordsSearcher {
 	
 	public static void main(String[] args) {
 		
-		String searched = "is";
+		String input = "yao";
+		String searched = input.toUpperCase();
 		
+		System.out.printf("Y) searched: %s\n", searched);
 		
 		// directory where the input files reside
 		final String dir = "/home/rootroot/main/yao_j/workplace_java/insight/src/insight/text/";
 		
 		ArrayList<String> fileNames = new ArrayList<String>();
+		fileNames.add("file2.txt");
 		fileNames.add("file0.txt");
 		fileNames.add("file1.txt");
-		fileNames.add("file2.txt");
+		
 		
 		String fileName0 = fileNames.get(0).toString();
-//		System.out.printf("fileName0: %s%n", fileName0);
 		
 		String line;
 		ArrayList<String> text = new ArrayList<String>();
@@ -32,12 +34,9 @@ public class WordsSearcher {
 				String fileName = fileNames.get(i).toString();
 				reader = new BufferedReader(new FileReader(dir+fileName));
 				line = reader.readLine();
-				text.add( line );
+//				text.add( line );
 				while (line != null) {
-//					System.out.println(line);
 					text.add( line );
-//					String l_one = text.get(text.size()-1).toString();
-//					System.out.println(l_one);
 					line = reader.readLine(); // read next line
 				}
 				reader.close();
@@ -48,14 +47,47 @@ public class WordsSearcher {
 		
 		System.out.printf("Y) Result: text.size(): %d\n", text.size());
 		
+		int cnt = 0;
+		
 		for (int i = 0; i < text.size(); ++i) {
 			String l = text.get(i).toString();
-//			System.out.printf("l_i: %d, lin: %s\n", i,l);
+
+			String l_up = l.toUpperCase();
+			System.out.printf("l_up: %d, l_up: %s\n", i,l_up.toString());
 			
-			int idx = l.indexOf(searched, 0);
-			System.out.printf("l_i: %d, lin: %s\n", i,l);
-			System.out.printf("idx: %d, str: %s\n", idx,searched);
+			int idx = l_up.indexOf(searched, 0);
+			
+			StringBuilder sb = new StringBuilder(l);
+			
+			while (idx != -1) {
+				
+				Boolean should_record = true;
+				
+				if (idx > 0) 							// if it is not the first charater of the checking line
+					if (Character.isAlphabetic(l.charAt(idx-1)))
+						should_record = false;
+				
+				if (idx+searched.length() < l.length()-2) // if it is not the last charater of the checking line
+					if (Character.isAlphabetic(l.charAt(idx+searched.length()+1)))
+						should_record = false;
+				
+				if (should_record) {
+					++cnt;
+					System.out.printf("l_i: %d, lin: %s\n", i,l);
+					System.out.printf("idx: %d, str: %s\n", idx,searched);
+					
+					sb.replace(idx, idx+searched.length(), searched.toUpperCase());
+				}
+				
+//				idx = l.indexOf(searched, idx+searched.length());
+
+				idx = l_up.indexOf(searched, idx+searched.length());
+			}
+			System.out.printf("rep: %d, rep: %s\n", i,sb.toString());
+			
 		}
+		
+		System.out.printf("Summary): sub: %s, cnt: %d\n", searched,cnt);
 		
 	  }
 
