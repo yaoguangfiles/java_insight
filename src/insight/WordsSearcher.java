d5ed8037c1db25aa62a5cd1d3074fc70374d1a22
yaoguangfiles <yaoguangfiles@gmail.com>
2022-03-22 16:07:39 -0400

    commit

--- src/insight/WordsSearcher.java
+++ src/insight/WordsSearcher.java
@@ -4,93 +4,110 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WordsSearcher {
 	
 	public static void main(String[] args) {
 		
-		String input = "Russian";
-		String searched = input.toUpperCase();
+		while(true) {
 		
-		System.out.printf("Y) searched: %s\n", searched);
-		
-		// directory where the input files reside
-		final String dir = "/home/rootroot/main/yao_j/workplace_java/insight/src/insight/text/";
-		
-		ArrayList<String> fileNames = new ArrayList<String>();
-		fileNames.add("file2.txt");
-		fileNames.add("file0.txt");
-		fileNames.add("file1.txt");
-		
-		String line;
-		ArrayList<String> text = new ArrayList<String>();
-		
-		for (int i = 0; i < fileNames.size(); ++i) {
-			BufferedReader reader;
-			try {
-				String fileName = fileNames.get(i).toString();
-				reader = new BufferedReader(new FileReader(dir+fileName));
-				line = reader.readLine();
-				while (line != null) {
-					if (line.trim().length() > 0)
-						text.add( line );
-					
-					line = reader.readLine(); // read next line
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("\n-------------------------------\n");
+			System.out.println("Enter a word to be searched:");
+			String word = scanner.nextLine();
+			
+			if (word.equalsIgnoreCase("-1"))
+				break;
+			
+			System.out.printf("Y) word searched: %s\n", word);
+		      
+	//		String input = "Russian";
+	//		String searched = input.toUpperCase();
+			
+			String searched = word.toUpperCase();
+			
+			System.out.printf("Y) searched: %s\n", searched);
+			
+			// directory where the input files reside
+			final String dir = "/home/rootroot/main/yao_j/workplace_java/insight/src/insight/text/";
+			
+			ArrayList<String> fileNames = new ArrayList<String>();
+			fileNames.add("file2.txt");
+			fileNames.add("file0.txt");
+			fileNames.add("file1.txt");
+			
+			String line;
+			ArrayList<String> text = new ArrayList<String>();
+			
+			for (int i = 0; i < fileNames.size(); ++i) {
+				BufferedReader reader;
+				try {
+					String fileName = fileNames.get(i).toString();
+					reader = new BufferedReader(new FileReader(dir+fileName));
+					line = reader.readLine();
+					while (line != null) {
+						if (line.trim().length() > 0)
+							text.add( line );
+						
+						line = reader.readLine(); // read next line
+					}
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
-		}
-		
-		System.out.printf("Y) Result: text.size(): %d\n", text.size());
-		StringBuilder sb = new StringBuilder();
-		
-		int cnt = 0;
-		
-		for (int i = 0; i < text.size(); ++i) {
-			String l = text.get(i).toString();
-
-			String l_up = l.toUpperCase();
-//			System.out.printf("l_up: %d, l_up: %s\n", i,l_up.toString());
 			
-			int idx = l_up.indexOf(searched, 0);
+			System.out.printf("Y) Result: text.size(): %d\n", text.size());
+			StringBuilder sb = new StringBuilder();
 			
-//			StringBuilder sb = new StringBuilder(l);
-			sb.setLength(0);
-			sb.append(l);
+			int cnt = 0;
 			
-			while (idx != -1) {
+			for (int i = 0; i < text.size(); ++i) {
+				String l = text.get(i).toString();
+	
+				String l_up = l.toUpperCase();
+	//			System.out.printf("l_up: %d, l_up: %s\n", i,l_up.toString());
 				
-				Boolean should_record = true;
+				int idx = l_up.indexOf(searched, 0);
 				
-				if (idx > 0) 							// if it is not the first charater of the checking line
-					if (Character.isAlphabetic(l.charAt(idx-1)))
-						should_record = false;
+	//			StringBuilder sb = new StringBuilder(l);
+				sb.setLength(0);
+				sb.append(l);
 				
-				if (idx+searched.length() < l.length()-1) // if it is not the last charater of the checking line
-					if (Character.isAlphabetic(l.charAt(idx+searched.length())))
-						should_record = false;
-				
-				if (should_record) {
-					++cnt;
-//					System.out.printf("l_i: %d, lin: %s\n", i,l);
-//					System.out.printf("idx: %d, str: %s\n", idx,searched);
+				while (idx != -1) {
 					
-					sb.replace(idx, idx+searched.length(), searched.toUpperCase());
-//					System.out.printf("rep: %d, rep: %s\n", i,sb.toString());
+					Boolean should_record = true;
+					
+					if (idx > 0) 							// if it is not the first charater of the checking line
+						if (Character.isAlphabetic(l.charAt(idx-1)))
+							should_record = false;
+					
+					if (idx+searched.length() < l.length()-1) // if it is not the last charater of the checking line
+						if (Character.isAlphabetic(l.charAt(idx+searched.length())))
+							should_record = false;
+					
+					if (should_record) {
+						++cnt;
+	//					System.out.printf("l_i: %d, lin: %s\n", i,l);
+	//					System.out.printf("idx: %d, str: %s\n", idx,searched);
+						
+						sb.replace(idx, idx+searched.length(), searched.toUpperCase());
+	//					System.out.printf("rep: %d, rep: %s\n", i,sb.toString());
+					}
+					
+	//				idx = l.indexOf(searched, idx+searched.length());
+	
+					idx = l_up.indexOf(searched, idx+searched.length());
 				}
-				
-//				idx = l.indexOf(searched, idx+searched.length());
-
-				idx = l_up.indexOf(searched, idx+searched.length());
+	//			System.out.printf("rep: %d, rep: %s\n", i,sb.toString());
+				System.out.printf("%s\n", sb.toString());
 			}
-			System.out.printf("rep: %d, rep: %s\n", i,sb.toString());
 			
-		}
+			System.out.printf("\nSummary): sub: %s, cnt: %d\n\n\n", searched,cnt);
+			
+		  }
 		
-		System.out.printf("Summary): sub: %s, cnt: %d\n", searched,cnt);
-		
-	  }
+	}
 
 }
